0bd51629323623e87df682470227fedf35916e52
Zhao Yisen <zhaoyisen119@163.com>
2026-01-06 21:49:29 +0800

    saveEMP

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,6 +2,8 @@ package com.sky.context;
 
 public class BaseContext {
 
+    // ThreadLocal 用于存储当前线程的用户ID
+    // ThreadLocal 提供了线程局部变量，每个线程都有自己的独立变量副本，互不干扰
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
     public static void setCurrentId(Long id) {
